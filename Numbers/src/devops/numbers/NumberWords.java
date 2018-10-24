5dd8033c4d5b8fdf8cfff948279fa4c2b8ed30e1
Debabrata Pradhan <devmapit@gmail.com>
2018-10-24 22:11:16 +0530

    Update NumberWords.java

--- Numbers/src/devops/numbers/NumberWords.java
+++ Numbers/src/devops/numbers/NumberWords.java
@@ -40,7 +40,7 @@ public class NumberWords {
     " nineteen"
   };
 
-  public NumberToWords() {}
+  public NumberWords() {}
 
   private static String convertLessThanOneThousand(int number) {
     String soFar;
@@ -146,50 +146,5 @@ public class NumberWords {
 	  System.out.println("345 is converted to " + numberWord.convert(345));
 	  System.out.println("22 is converted to " + numberWord.convert(22));
 	  System.out.println("-1 is converted to " + numberWord.convert(-1));
-
-	  
-//    System.out.println(NumberToWords.convert(0));
-//    System.out.println(NumberToWords.convert(1));
-//    System.out.println(NumberToWords.convert(16));
-//    System.out.println(NumberToWords.convert(100));
-//    System.out.println(NumberToWords.convert(118));
-//    System.out.println(NumberToWords.convert(200));
-//    System.out.println(NumberToWords.convert(219));
-//    System.out.println(NumberToWords.convert(800));
-//    System.out.println(NumberToWords.convert(801));
-//    System.out.println(NumberToWords.convert(1316));
-//    System.out.println(NumberToWords.convert(1000000));
-//    System.out.println(NumberToWords.convert(2000000));
-//    System.out.println(NumberToWords.convert(3000200));
-//    System.out.println(NumberToWords.convert(700000));
-//    System.out.println(NumberToWords.convert(9000000));
-//    System.out.println(NumberToWords.convert(9001000));
-//    System.out.println(NumberToWords.convert(123456789));
-//    System.out.println(NumberToWords.convert(2147483647));
-//    System.out.println(NumberToWords.convert(3000000010L));
-
-    /*
-     *** zero
-     *** one
-     *** sixteen
-     *** one hundred
-     *** one hundred eighteen
-     *** two hundred
-     *** two hundred nineteen
-     *** eight hundred
-     *** eight hundred one
-     *** one thousand three hundred sixteen
-     *** one million
-     *** two millions
-     *** three millions two hundred
-     *** seven hundred thousand
-     *** nine millions
-     *** nine millions one thousand
-     *** one hundred twenty three millions four hundred
-     **      fifty six thousand seven hundred eighty nine
-     *** two billion one hundred forty seven millions
-     **      four hundred eighty three thousand six hundred forty seven
-     *** three billion ten
-     **/
   }
 }
